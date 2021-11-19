de97703272ec50d66d4a449b072cf48e9a8066a7
Spencer Farley <2847259+farlee2121@users.noreply.github.com>
2021-11-19 14:15:24 -0600

    setup basic build and test

--- test/Core.Tests/Tests.fs
+++ test/Core.Tests/Tests.fs
@@ -1,8 +1,16 @@
 module Tests
 
 open System
-open Xunit
+open Expecto
 
-[<Fact>]
-let ``My test`` () =
-    Assert.True(true)
+[<Tests>]
+let tests =
+  testList "samples" [
+    testCase "universe exists (╭ರᴥ•́)" <| fun _ ->
+      let subject = true
+      Expect.isTrue subject "I compute, therefore I am."
+
+    testCase "when true is not (should fail)" <| fun _ ->
+      let subject = false
+      Expect.isTrue subject "I should fail because the subject is false"
+  ]
