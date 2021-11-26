ecc358a38a03102e57cca1a4ff2a953e936c6237
Spencer Farley <2847259+farlee2121@users.noreply.github.com>
2021-11-26 17:01:27 -0600

    Convert tree implementation to F#

---
+++ src/Core/TreeUtils.fs
@@ -0,0 +1,22 @@
+﻿namespace Notedown.Core
+module TreeUtils =
+    open System.Collections.Generic 
+
+    type Queue<'T> with
+        member self.EnqueueRange(range) = 
+            for element in range do
+                (self.Enqueue(element))
+
+    let fold rootNode getChildren aggFn initialValue =
+        // NOTE: tail recursion optimization seems a bit unreliable in F#, and this has potential for deep nesting. Thus I'm solving this iteratively
+        let mutable aggregate = initialValue;
+        let mutable unwalked = Queue()
+        unwalked.Enqueue(rootNode)
+        
+        while not (Seq.isEmpty unwalked) do
+            let node = unwalked.Dequeue()
+            unwalked.EnqueueRange (getChildren node)
+
+            aggregate <- aggFn aggregate node
+        
+        aggregate
--- test/Core.Tests/TreeUtilityTests.fs
+++ test/Core.Tests/TreeUtilityTests.fs
@@ -16,24 +16,6 @@ let getChildren node =
             | InternalNode (_, children) -> children 
             | LeafNode _ -> []
 
-module csharp =
-  let toFunc<'a, 'b> f =
-      System.Func<'a, 'b> f
-  let toFunc2<'a, 'b, 'c> f =
-      System.Func<'a, 'b, 'c> f
-  let toAction<'a> f =
-      System.Action<'a> f
-  let toAction2<'a, 'b> f =
-      System.Action<'a, 'b> f
-
-
-module Tree = 
-  let walk<'node> root fGetChildren fNode = Notedown.Core.Tree.Walk<'node>(root, fGetChildren |> csharp.toFunc, fNode |> csharp.toAction)
-  let collect<'node> root fGetChildren ftest = Notedown.Core.Tree.Collect<'node>(root, fGetChildren |> csharp.toFunc, ftest |> csharp.toFunc)
-  let fold<'node, 'agg> root fGetChildren fFold initial = Notedown.Core.Tree.Fold<'node, 'agg>(root, fGetChildren |> csharp.toFunc, fFold |> csharp.toFunc2, initial)
-
-
-
 
 [<Tests>]
 let treeUtilityTests =
@@ -41,14 +23,12 @@ let treeUtilityTests =
     test "Basic tree flatten" {
         let tree = branch [leaf 1; branch [leaf 2; leaf 3]; branch [leaf 4; branch [leaf 5; leaf 6]]]
 
-        
-
         let flatten agg node =
           match node with
-          | LeafNode value -> Seq.append agg [value]
+          | LeafNode value -> List.append agg [value]
           | InternalNode _ -> agg
 
-        let actualLeaves = Tree.fold tree getChildren flatten []
+        let actualLeaves = TreeUtils.fold tree getChildren flatten []
 
         Expect.equal actualLeaves [1;2;3;4;5;6] ""
     }
