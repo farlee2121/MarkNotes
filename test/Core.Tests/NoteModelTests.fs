98559c275a70ce76d2b9118905e20e13e48e3931
Spencer Farley <2847259+farlee2121@users.noreply.github.com>
2023-04-13 10:28:08 -0500

    Reorganize test lists based on behaviors of members

--- test/Core.Tests/NoteModelTests.fs
+++ test/Core.Tests/NoteModelTests.fs
@@ -73,217 +73,220 @@ let metadataModelTests = testList "Note Model" [
 
             expected =! actual
 
-    testProperty'
-        "should return a empty meta but all contents when given a document with no frontmatter"
-        <| fun (document: NoHeadingsString) ->
-            string
-            let expected = {
-                Level = SectionLevel.Root
-                Meta = MetadataValue.default'
-                ExclusiveText = document.Get
-                Children = []
-            }
+    testList "Meta" [
+        testProperty'
+            "should return a empty meta but all contents when given a document with no frontmatter"
+            <| fun (document: NoHeadingsString) ->
+                string
+                let expected = {
+                    Level = SectionLevel.Root
+                    Meta = MetadataValue.default'
+                    ExclusiveText = document.Get
+                    Children = []
+                }
 
-            let actual = NoteModel.parse document.Get
+                let actual = NoteModel.parse document.Get
 
-            expected =! actual
-            document.Get =! actual.Text() 
+                expected =! actual
+                document.Get =! actual.Text() 
 
-    testCase
-        "should parse simple key-value meta"
-        <| fun () ->
-            let expectedKey = "rating";
-            let expectedValue = 5;
-            let document =
-                $"\
-                ---\n\
-                  {expectedKey}: {expectedValue}\n\
-                ---\n\
-                "
+        testCase
+            "should parse simple key-value meta"
+            <| fun () ->
+                let expectedKey = "rating";
+                let expectedValue = 5;
+                let document =
+                    $"\
+                    ---\n\
+                      {expectedKey}: {expectedValue}\n\
+                    ---\n\
+                    "
 
-            let expected = {
-                Level = SectionLevel.Root
-                Meta = sdict [expectedKey, (expectedValue |> string |> SingleValue)] |> Complex
-                ExclusiveText = document
-                Children = []
-            }
+                let expected = {
+                    Level = SectionLevel.Root
+                    Meta = sdict [expectedKey, (expectedValue |> string |> SingleValue)] |> Complex
+                    ExclusiveText = document
+                    Children = []
+                }
 
-            let actual = NoteModel.parse document
+                let actual = NoteModel.parse document
 
-            expected =! actual
+                expected =! actual
 
-    testCase
-        "should parse array meta"
-        <| fun () ->
-            let document =
-                "\
-                ---\n\
-                  author: [Spencer, David, Joe]\n\
-                ---\n\
-                "
+        testCase
+            "should parse array meta"
+            <| fun () ->
+                let document =
+                    "\
+                    ---\n\
+                      author: [Spencer, David, Joe]\n\
+                    ---\n\
+                    "
 
-            let expected = {
-                Level = SectionLevel.Root
-                Meta = sdict ["author", (["Spencer"; "David"; "Joe"] |> List.map SingleValue |> MetadataValue.Vector)] |> Complex
-                ExclusiveText = document
-                Children = []
-            }
+                let expected = {
+                    Level = SectionLevel.Root
+                    Meta = sdict ["author", (["Spencer"; "David"; "Joe"] |> List.map SingleValue |> MetadataValue.Vector)] |> Complex
+                    ExclusiveText = document
+                    Children = []
+                }
 
-            let actual = NoteModel.parse document
+                let actual = NoteModel.parse document
 
-            expected =! actual
+                expected =! actual
 
-    testCase
-        "should parse heterogeneous arrays of meta"
-        <| fun () ->
-            let document =
-                "\
-                ---\n\
-                  author: [Spencer, [David], {hi: 5}]\n\
-                ---\n\
-                "
+        testCase
+            "should parse heterogeneous arrays of meta"
+            <| fun () ->
+                let document =
+                    "\
+                    ---\n\
+                      author: [Spencer, [David], {hi: 5}]\n\
+                    ---\n\
+                    "
 
-            let expected = {
-                Level = SectionLevel.Root
-                Meta = sdict [
-                    "author", Vector [
-                        SingleValue "Spencer"
-                        Vector [ SingleValue "David"]
-                        Complex (sdict ["hi", SingleValue "5"])
-                    ]
-                ] |> Complex
-                ExclusiveText = document
-                Children = []
-            }
-
-            let actual = NoteModel.parse document
-
-            expected =! actual
-
-    testCase
-        "should parse nested maps of meta"
-        <| fun () ->
-            // beware the funky spacing required here
-            // \n  \  is adding two spaces in front of the nested properties then ignoring space just for code alignment.
-            // Not sure how to make this more intuitive since F# doesn't support relative formatting like C#'s triple quote.
-            // F# triple quote is just a literal string that ignores escape sequences
-            let document =
-                "\
-                ---\n\
-                  author: Spencer\n\
-                  config: \n  \
-                    foo: 5\n  \
-                    bar: {baz: 8}\n\
-                ---\n\
-                "
-
-            let expected = {
-                Level = SectionLevel.Root
-                Meta = MetadataValue.fromPairs [
-                    "author", SingleValue "Spencer"
-                    "config", MetadataValue.fromPairs [
-                        "foo", SingleValue "5"
-                        "bar", MetadataValue.fromPairs [
-                            "baz", SingleValue "8"
-                        ] 
-                    ]
-                ]
-                ExclusiveText = document
-                Children = []
-            }
-
-            let actual = NoteModel.parse document
-
-            expected =! actual
-
-    testCase
-        "should parse headings without a codeblock as sections with no meta (other than what's inherited)"
-        <| fun () ->
-            // TODO: maybe remake this as a property. Probably need to register a custom Arb
-            let document =
-                "\
-                # Title\
-                "
-
-            let expected = {
-                Level = SectionLevel.Root
-                Meta = MetadataValue.default'
-                ExclusiveText = ""
-                Children = [
-                    {
-                        Level = SectionLevel.Heading 1
-                        ExclusiveText = document
-                        Meta = MetadataValue.default'
-                        Children = []
-                    }
-                ]
-            }
-
-            let actual = NoteModel.parse document
-
-            expected =! actual
-
-    testCase
-        "should parse yaml code blocks under headers as meta"
-        <| fun () ->
-            // TODO: maybe remake this as a property. Probably need to register a custom Arb
-            let document =
-                "\
-                # Title \n\
-                ```yml\n\
-                  rating: 5\n\
-                ```\
-                "
-
-            let expected = {
-                Level = SectionLevel.Root
-                Meta = MetadataValue.default'
-                ExclusiveText = ""
-                Children = [
-                    {
-                        Level = SectionLevel.Heading 1
-                        ExclusiveText = document
-                        Meta = MetadataValue.fromPairs [
-                            "rating", SingleValue "5"
+                let expected = {
+                    Level = SectionLevel.Root
+                    Meta = sdict [
+                        "author", Vector [
+                            SingleValue "Spencer"
+                            Vector [ SingleValue "David"]
+                            Complex (sdict ["hi", SingleValue "5"])
                         ]
-                        Children = []
-                    }
-                ]
-            }
+                    ] |> Complex
+                    ExclusiveText = document
+                    Children = []
+                }
 
-            let actual = NoteModel.parse document
+                let actual = NoteModel.parse document
 
-            expected =! actual
+                expected =! actual
 
-    testCase
-        "should not parse code blocks not marked as yaml or yml"
-        <| fun () ->
-            // TODO: maybe remake this as a property. Probably need to register a custom Arb
-            let document =
-                "\
-                # Title \n\
-                ```cs\n\
-                  rating: 5\n\
-                ```\
-                "
+        testCase
+            "should parse nested maps of meta"
+            <| fun () ->
+                // beware the funky spacing required here
+                // \n  \  is adding two spaces in front of the nested properties then ignoring space just for code alignment.
+                // Not sure how to make this more intuitive since F# doesn't support relative formatting like C#'s triple quote.
+                // F# triple quote is just a literal string that ignores escape sequences
+                let document =
+                    "\
+                    ---\n\
+                      author: Spencer\n\
+                      config: \n  \
+                        foo: 5\n  \
+                        bar: {baz: 8}\n\
+                    ---\n\
+                    "
 
-            let expected = {
-                Level = SectionLevel.Root
-                Meta = MetadataValue.default'
-                ExclusiveText = ""
-                Children = [
-                    {
-                        Level = SectionLevel.Heading 1
-                        ExclusiveText = document
-                        Meta = MetadataValue.default'
-                        Children = []
-                    }
-                ]
-            }
+                let expected = {
+                    Level = SectionLevel.Root
+                    Meta = MetadataValue.fromPairs [
+                        "author", SingleValue "Spencer"
+                        "config", MetadataValue.fromPairs [
+                            "foo", SingleValue "5"
+                            "bar", MetadataValue.fromPairs [
+                                "baz", SingleValue "8"
+                            ] 
+                        ]
+                    ]
+                    ExclusiveText = document
+                    Children = []
+                }
 
-            let actual = NoteModel.parse document
+                let actual = NoteModel.parse document
 
-            expected =! actual
+                expected =! actual
+
+        testCase
+            "should parse headings without a codeblock as sections with no meta (other than what's inherited)"
+            <| fun () ->
+                // TODO: maybe remake this as a property. Probably need to register a custom Arb
+                let document =
+                    "\
+                    # Title\
+                    "
+
+                let expected = {
+                    Level = SectionLevel.Root
+                    Meta = MetadataValue.default'
+                    ExclusiveText = ""
+                    Children = [
+                        {
+                            Level = SectionLevel.Heading 1
+                            ExclusiveText = document
+                            Meta = MetadataValue.default'
+                            Children = []
+                        }
+                    ]
+                }
+
+                let actual = NoteModel.parse document
+
+                expected =! actual
+
+        testCase
+            "should parse yaml code blocks under headers as meta"
+            <| fun () ->
+                // TODO: maybe remake this as a property. Probably need to register a custom Arb
+                let document =
+                    "\
+                    # Title \n\
+                    ```yml\n\
+                      rating: 5\n\
+                    ```\
+                    "
+
+                let expected = {
+                    Level = SectionLevel.Root
+                    Meta = MetadataValue.default'
+                    ExclusiveText = ""
+                    Children = [
+                        {
+                            Level = SectionLevel.Heading 1
+                            ExclusiveText = document
+                            Meta = MetadataValue.fromPairs [
+                                "rating", SingleValue "5"
+                            ]
+                            Children = []
+                        }
+                    ]
+                }
+
+                let actual = NoteModel.parse document
+
+                expected =! actual
+
+        testCase
+            "should not parse code blocks not marked as yaml or yml"
+            <| fun () ->
+                // TODO: maybe remake this as a property. Probably need to register a custom Arb
+                let document =
+                    "\
+                    # Title \n\
+                    ```cs\n\
+                      rating: 5\n\
+                    ```\
+                    "
+
+                let expected = {
+                    Level = SectionLevel.Root
+                    Meta = MetadataValue.default'
+                    ExclusiveText = ""
+                    Children = [
+                        {
+                            Level = SectionLevel.Heading 1
+                            ExclusiveText = document
+                            Meta = MetadataValue.default'
+                            Children = []
+                        }
+                    ]
+                }
+
+                let actual = NoteModel.parse document
+
+                expected =! actual
+
+    ]
 
     testList "should parse heading hierarchy" [
         
@@ -400,9 +403,9 @@ let metadataModelTests = testList "Note Model" [
                 expected =! actual
     ]
 
-    testList "should keep section content exclusive" [
+    testList "ExclusiveContents" [
         testCase
-            "should keep conten exclusive for sibling sections"
+            "should not overlap for sibling sections"
             <| fun () ->
                 let sectionsText = [
                     String.joinLines [
@@ -438,7 +441,7 @@ let metadataModelTests = testList "Note Model" [
                 document =! Section.fullText actual
 
         testCase
-            "should keep content exclusive between parent and child"
+            "should not overlap between parent and child"
             <| fun () ->
                 let sectionsText = [
                     String.joinLines [
@@ -473,7 +476,7 @@ let metadataModelTests = testList "Note Model" [
                 document =! Section.fullText actual
 
         testCase
-            "should keep content exclusive between root and headings"
+            "should not overlap between root and headings"
             <| fun () ->
                 let sectionsText = [
                     String.joinLines [
@@ -505,6 +508,10 @@ let metadataModelTests = testList "Note Model" [
 
                 expected =! actual
                 document =! Section.fullText actual
+
+    ]
+
+    testList "Meta Inheritance" [
 
     ]
 ]
